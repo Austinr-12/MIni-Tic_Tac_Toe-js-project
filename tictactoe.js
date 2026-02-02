5f045470f04df3757ac69028ad66c7d3758c865a
Austin <austin.ramirez1@outlook.com>
2026-02-01 19:48:12 -0600

    player one and gameActive status and printBoard Function

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,15 @@
 const prompt = require("prompt-sync")();
 
 let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+let currentPlayer = "🐐";
+let gameActive = true;
+
+function printBoard() {
+  console.log(`
+    ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
+    ---------
+    ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
+    ---------
+    ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
+  `);
+}
